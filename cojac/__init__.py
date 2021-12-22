518cd626827b672c634c55d50ac25d75c8af6acd
kpj <kim.philipp.jablonski@gmail.com>
2021-12-22 11:40:24 +0100

    Start following basic Python package structure

---
+++ cojac/__init__.py
@@ -0,0 +1,14 @@
+from .cooc_colourmut import main as cooc_colourmut
+from .cooc_mutbamscan import main as cooc_mutbamscan
+from .cooc_pubmut import main as cooc_pubmut
+from .cooc_tabmut import main as cooc_tabmut
+from .phe2cojac import main as phe2cojac
+
+
+__all__ = [
+    "cooc_colourmut",
+    "cooc_mutbamscan",
+    "cooc_pubmut",
+    "cooc_tabmut",
+    "phe2cojac",
+]
