11eb7f97bf3969c10bd49c668d3f36462d407dab
kpj <kim.philipp.jablonski@gmail.com>
2022-02-16 17:48:08 +0100

    Add basic structure of integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,33 @@
+import subprocess
+
+
+def test_workflow():
+    # download data (TODO: use better sources)
+    data_sources = [
+        "https://polybox.ethz.ch/index.php/s/vncvlX4pAGo4h33/download?path=%2F&files=REF_aln.bam",
+        "https://polybox.ethz.ch/index.php/s/vncvlX4pAGo4h33/download?path=%2F&files=sam1.bam",
+        "https://polybox.ethz.ch/index.php/s/vncvlX4pAGo4h33/download?path=%2F&files=sam1.bam.bai",
+        "https://polybox.ethz.ch/index.php/s/vncvlX4pAGo4h33/download?path=%2F&files=sam2.bam",
+        "https://polybox.ethz.ch/index.php/s/vncvlX4pAGo4h33/download?path=%2F&files=sam2.bam.bai",
+    ]
+
+    for url in data_sources:
+        name = url.split("=")[-1]
+        print(name)
+        subprocess.run([
+            "wget",
+            "--no-clobber",
+            "-O", name,
+            url
+        ])
+
+    subprocess.run([
+        "wget",
+        "--no-clobber",
+        "https://raw.githubusercontent.com/phe-genomics/variant_definitions/main/variant_yaml/imagines-viewable.yml"
+    ])
+
+    # retrieve variants
+    subprocess.run([
+        "cojac", "phe2cojac", "--shortname", 'om2', "--yaml", "omicron_ba2_mutations.yaml", "imagines-viewable.yml"
+    ])
